dc006de3a9bf3d166927666e8854971099bfa542
ryanitj <ryanitj@gmail.com>
2021-07-01 18:27:26 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createIconSetFromIcoMoon } from '@expo/vector-icons';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+
+
+const Icon = createIconSetFromIcoMoon(
+  require('./selection.json'),
+  'IcoMoon',
+  'icomoon.ttf'
+);
+
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ IcoMoon: require('./assets/fonts/icomoon.ttf') });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Icon name="pencil2" size={80} color="#bf1313"></Icon>
       <StatusBar style="auto" />
     </View>
   );
